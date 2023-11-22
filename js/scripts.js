80a65bc59a25ee4992754e5178cffc5d0477fd7d
VlasStas <stas-vlas@ukr.net>
2023-11-22 22:25:25 +0200

    update
    js and html

--- js/scripts.js
+++ js/scripts.js
@@ -405,11 +405,148 @@
 // console.log(summ);
 // document.write(summ);
 
-let x = 10;
-let y = 20;
-const foo = (z) => {
-  let x = 100;
-  return x + y + z;
-};
-// foo(30);
-console.log(foo(30));
+// ===============================
+// =================================
+// 24
+// Функції
+
+// function showMessage(name, city) {
+//   console.log("Hello, my name is " + name + ". I'm from " + city);
+// }
+// showMessage("Nata", "Kyiv");
+// showMessage("Olya", "Lviv");
+// showMessage("Katya", "Dnipro");
+
+// const saiHi = function () {
+//   console.log("Hi!");
+// };
+
+// saiHi();
+
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// let result = sum(7, 9);
+
+// console.log(result);
+
+// const testFunk = function (a, b) {
+//   console.log("a:", a);
+//   console.log("b:", b);
+
+//   return;
+// };
+
+// testFunk(1, 3);
+
+// аргументи і rest ==============
+
+// const addName = function () {
+//   const arg = Array.from(arguments);
+//   //   console.log(arguments);
+//   console.log(arg);
+// };
+
+// addName(1, 2, 3, 5, 6, 9);
+
+// const addName = function (...arg) {
+//   console.log(arg);
+// };
+
+// addName(1, 2, 3, 5, 6, 9);
+
+// callback ==================
+
+// function ask(question, yes, no) {
+//   if (confirm(question)) {
+//     yes();
+//   } else {
+//     no();
+//   }
+// }
+
+// function showOk() {
+//   console.log("You say ok");
+// }
+
+// function showNo() {
+//   console.log("You say No");
+// }
+
+// ask("Yes or no?", showOk, showNo);
+
+// hof =====================
+
+// const hello = function (name) {
+//   console.log(`Hello, ${name}!`);
+// };
+
+// // hello("Ann");
+// const searchName = function (callback) {
+//   const name = "Ivan";
+//   callback(name);
+// };
+
+// searchName(hello);
+
+// function checkAge(age) {
+//   if (age > 18) {
+//     return console.log("Hello");
+//   }
+//   console.log("Good Bye!");
+// }
+
+// checkAge(17);
+
+// function showMovie(age) {
+//   if (age < 21) {
+//     return;
+//   }
+//   return console.log("Go to movie!");
+// }
+// showMovie(21);
+
+// Стрілкові функції =================
+
+// const test = (arg) => {
+//   console.log(arg);
+// };
+// test("Hello!");
+
+// const ahowMovie = (age) => {
+//   if (age >= 21) {
+//     return console.log("Hello!");
+//   }
+//   return console.log("Bye!");
+// };
+// ahowMovie(20);
+
+// const a = () => {
+//   console.log("a");
+// };
+
+// const b = () => {
+//   console.log("b");
+// };
+
+// const c = () => {
+//   console.log("c");
+//   a();
+//   b();
+// };
+
+// c();
+
+// console.log(document);
+
+// const addNumber = (number) => {
+//   let summ = Number(number) + 10;
+//   return console.log(summ);
+// };
+
+// const numberRef = document.querySelector('input[name="number"]');
+
+// const buttonRef = document.querySelector("button");
+
+// buttonRef.addEventListener("click", () => addNumber(numberRef.value));
