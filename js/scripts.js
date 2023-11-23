b2c9fd13735aac4be0c9d9f14c8e5ba3220f3f26
VlasStas <stas-vlas@ukr.net>
2023-11-23 15:32:02 +0200

    update
    js html

--- js/scripts.js
+++ js/scripts.js
@@ -550,3 +550,130 @@
 // const buttonRef = document.querySelector("button");
 
 // buttonRef.addEventListener("click", () => addNumber(numberRef.value));
+
+// ============================
+// ---------------------------
+// ===============================================
+//
+// Замикання
+// -------------------------------------------------
+
+// function createNewSum(n) {
+//   return function () {
+//     console.log(10 * n);
+//   };
+// }
+// const calc = createNewSum(5);
+
+// calc();
+
+// function createNewNumber(n) {
+//   return function (num) {
+//     return n + num;
+//   };
+// }
+
+// const addFive = createNewNumber(5);
+
+// console.log(addFive(10));
+
+// function createUrl(domain) {
+//   return function (url) {
+//     return `http://${url}.${domain}`;
+//   };
+// }
+
+// const comUrl = createUrl("com");
+// const comUa = createUrl("ua");
+
+// console.log(comUrl("google"));
+// console.log(comUrl("fb"));
+
+// console.log(comUa("google"));
+// console.log(comUa("fb"));
+
+// ================================
+// this
+// ===========================
+
+// function hello() {
+//   console.log("hello", this);
+// }
+// hello();
+
+// const user = {
+//   name: "Ivan",
+//   city: "Odessa",
+//   sayHello: hello,
+// };
+
+// user.sayHello();
+// -------------------
+
+// function abc() {
+//   console.log("В функції");
+//   console.log(this);
+// }
+// abc();
+
+// document.querySelector("p").onclick = abc;
+
+// function changeColor() {
+//   console.log(this);
+//   this.style.background = "green";
+// }
+
+// document.querySelector("div").onclick = changeColor;
+
+// function changeColor() {
+//   this.style.background = "green";
+// }
+
+// let user = document.querySelectorAll("div");
+
+// user.forEach(function (element) {
+//   element.onclick = changeColor;
+// });
+
+// ---------------
+
+// const showList = () => {
+//   console.log(this);
+// };
+// showList();
+
+// const list = {
+//   names: ["Ann", "Olga", "Nata"],
+//   showList: showList,
+// };
+// list.showList();
+// ========================
+
+// function hello() {
+//   console.log(this);
+// }
+
+// const user = {
+//   name: "Ivan",
+//   age: 30,
+//   hello: hello,
+//   sayHelloWindow: hello.bind(window),
+//   info: function (city) {
+//     console.log(`Name is ${this.name}`);
+//     console.log(`Age is ${this.age}`);
+//     console.log(`City is ${city}`);
+//   },
+// };
+
+// const Ann = {
+//   name: "Anna",
+//   age: 23,
+// };
+// const Nata = {
+//   name: "Natalia",
+//   age: 50,
+// };
+
+// user.info.bind(Ann)("Kyiv");
+// const nataInfo = user.info.bind(Nata, "Dnipro");
+// nataInfo();
